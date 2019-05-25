299ef4c668682eb29c318442b7b5ae4eedd288e8
re-jon <r_jonach@outlook.com>
2019-05-25 19:01:58 +0200

    Pyth11u12

--- Pyth/hw2a.py
+++ Pyth/hw2a.py
@@ -1,7 +1,7 @@
 
-#Homework Digital Humanities, Lesson 8, Code and Comments by Regina Jonach
+#Homework Digital Humanities, Lesson 8, Code and Comments by Regina Jonach (Python3)
 
-#This#This Python file uses the following encoding: utf-8
+
 import re
 import os, sys                                       
                                         
@@ -14,21 +14,14 @@ for f in listOfFiles:
 	newList = (pathToFolder+f)
 	with open(newList, "rt", encoding="utf-8") as af:
 		data = af.read()
-		#newData = re.search(r"[\D<div3\s\w+\D\D\article]([^<]+)[\D</div3>]", data).group()
 
 
 		newData = re.sub(r"\D<div3\s\w+\D\D\article\D\s\w\D\D\d\d\D\s\w+\D\D\w+\D\s\w+\D\D\w+\D\D(\D>\s.+\s\D<) \D</div3>", "", data)
 	
-		
-	
-
-
-	
-
 
 	newFileName = newList + "_modified_article.xml"         
 	with open(newFileName, "wt", encoding="utf-8") as afe:
 		afe.write(str(newData))
 
 
-#newData = re.sub("<div3 type="article"*>([^<]</div3>", "", data)      
+     
